c92ca27ee351cd1e950b970ad2a69f0b6c425155
oyershov <oyershov@kaaiot.io>
2021-03-15 18:02:32 +0200

    test

--- cmd/nats_client.go
+++ cmd/nats_client.go
@@ -2,14 +2,41 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/nats-io/nats.go@v1.10.0"
+	nats "github.com/nats-io/nats.go"
 )
 
 func main() {
-	nc, err := nats.Connect("demo.nats.io")
+	nc, err := nats.Connect("nats://localhost:4222")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer nc.Close()
+
+	// Channel Subscriber
+	ch := make(chan *nats.Msg, 64)
+	sub, err := nc.ChanSubscribe("kaa.v1.events.%s.endpoint.traffic-reporting.payload", ch)
+
+	var Stop = make(chan os.Signal, 1)
+	signal.Notify(Stop,
+		syscall.SIGTERM,
+		syscall.SIGINT,
+		// syscall.SIGKILL,
+		syscall.SIGABRT,
+	)
+
+	for {
+		select {
+		case msg := <-ch:
+			log.Println(msg)
+		case <-Stop:
+			// Unsubscribe
+			sub.Unsubscribe()
+			// Drain
+			sub.Drain()
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,16 @@
-module github.com/alex60217101990/kpc_utils
+module github.com/alex60217101990/kpc_utils.git
 
 go 1.14
 
 require (
+	github.com/alex60217101990/kpc_utils v0.0.0-20210315153816-c238bcb365c1 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/klauspost/compress v1.11.12 // indirect
+	github.com/minio/highwayhash v1.0.1 // indirect
 	github.com/nats-io/jwt v1.2.2 // indirect
-	github.com/nats-io/nats.go v1.10.0 // indirect
+	github.com/nats-io/jwt/v2 v2.0.1 // indirect
+	github.com/nats-io/nats-server/v2 v2.1.9 // indirect
+	github.com/nats-io/nats.go v1.10.0
 	github.com/nats-io/nkeys v0.3.0 // indirect
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
 )
